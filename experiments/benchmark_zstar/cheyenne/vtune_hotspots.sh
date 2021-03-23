cd35655726a8e16c4e5adf42077027919ccb2a33
Jackson Vanover <jacksonvanover@gmail.com>
2021-03-23 14:00:31 -0600

    added save mechanism for generated vtune report

--- experiments/benchmark_zstar/cheyenne/vtune_hotspots.sh
+++ experiments/benchmark_zstar/cheyenne/vtune_hotspots.sh
@@ -16,6 +16,10 @@ amplxe-cl --collect=hotspots --result-dir=result --return-app-exitcode -- mpiexe
 err="${?}"
 if [ "${err}" -eq 0 ]; then
     amplxe-cl --report=gprof-cc --result-dir=result --format=text --report-output=result.txt
+    pushd prose_logs > /dev/null 2>&1
+    dest="$(printf "./prose_logs/%03d/report.txt" $(($(find -type d -regex "./[0-9]+$" | wc -l) - 2)))"
+    popd > /dev/null 2>&1
+    cp result.txt "${dest}"
     exit 0
 else
     exit 1
