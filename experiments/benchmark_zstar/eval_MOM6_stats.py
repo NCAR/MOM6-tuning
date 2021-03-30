3da8ad4e86f4f351abb1fe09177ddd6588245bd6
Jackson Vanover <jacksonvanover@gmail.com>
2021-03-29 20:05:28 -0600

    modified to pass logpath to eval

--- experiments/benchmark_zstar/eval_MOM6_stats.py
+++ experiments/benchmark_zstar/eval_MOM6_stats.py
@@ -1,18 +1,12 @@
 from cheyenne.read_amplxe_results import read_amplxe_results
-
-def read_MOM6_time(logfilepath, clockname):
-    logfile = open(logfilepath, 'r')
-    for line in logfile:
-        if line[0:len(clockname)]==clockname:
-            return float(line.split()[-1])
-    raise RuntimeError("Couldn't find clock entry in logfile")
-
+import sys
+import os
 
 def get_MOM_continuity_PPM_time():
 
     totalTime = 0.0
 
-    routines = read_amplxe_results("./result.txt")
+    routines = read_amplxe_results(os.path.join(logPath,"vtune_report.txt"))
 
     for r in [routines[r] for r in routines]:
         if r.name.startswith("mom_continuity_ppm"):
@@ -22,6 +16,6 @@ def get_MOM_continuity_PPM_time():
 
 
 if __name__ == "__main__":
-    # clock_continuity = read_MOM6_time("./logfile.000000.out", "(Ocean continuity PPM)")
-    clock_continuity = get_MOM_continuity_PPM_time()
+    logPath = sys.argv[1]
+    clock_continuity = get_MOM_continuity_PPM_time(logPath)
     print(clock_continuity)
